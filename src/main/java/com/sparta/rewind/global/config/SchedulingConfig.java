e8c94f19df87abe5d9650d08283e67e1adc41cdc
dmlal <opkl8781@naver.com>
2023-12-25 01:56:27 +0900

    feat : UTC+9 타임존 설정

--- src/main/java/com/sparta/rewind/global/config/SchedulingConfig.java
+++ src/main/java/com/sparta/rewind/global/config/SchedulingConfig.java
@@ -1,10 +1,16 @@
 package com.sparta.rewind.global.config;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @EnableScheduling
 @Configuration
 public class SchedulingConfig {
-
+    @PostConstruct
+    public void timezoneSetting() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 }
