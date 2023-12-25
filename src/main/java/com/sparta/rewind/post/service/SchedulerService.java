fb166c24e8611679f6491b7c09b7d47aa1a68a59
dmlal <opkl8781@naver.com>
2023-12-25 14:05:10 +0900

    refactor :90일 스케줄러 추가 및 변수명 변경

--- src/main/java/com/sparta/rewind/post/service/SchedulerService.java
+++ src/main/java/com/sparta/rewind/post/service/SchedulerService.java
@@ -12,15 +12,25 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class SchedulerService {
 
-    private final PostService postService;
     private final PostRepository postRepository;
 
 
-    // 90일은 너무 깁니다. 30분으로 하시죠   1분마다 30분이 지난 게시물을 지웁니다.
+    /*
+    90일은 너무 깁니다. 30분으로 하시죠
+    1분마다 30분이 지난 게시물을 지웁니다.
+    (원활한 테스트를 위해 30분으로 설정하였습니다.)
+     */
     @Transactional
     @Scheduled(cron = "0 * * * * *")
     public void deleteOldPost() {
-        LocalDateTime before30MinutesAgo = LocalDateTime.now().minusMinutes(30);
-        postRepository.deleteByCreatedAtBefore(before30MinutesAgo);
+        LocalDateTime postHalfHourAgo = LocalDateTime.now().minusMinutes(30);
+        postRepository.deleteByCreatedAtBefore(postHalfHourAgo);
     }
+
+    // 90일이 지난 게시물을 자정마다 지우는 진짜 스케줄러
+//    @Scheduled(cron = "0 0 0 * * *")
+//    public void deleteOldPost() {
+//        LocalDateTime post90DaysAgo = LocalDateTime.now().minusDays(90);
+//        postRepository.deleteByCreatedAtBefore(post90DaysAgo);
+//    }
 }
